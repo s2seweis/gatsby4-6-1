25be1e15fec717f14965a01e3c987f4dc1acede1
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-04-01 19:03:53 +0800

    rebuild the gatsby portfolio to gatsby v4

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,29 +1,25 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import BlocksRenderer from "../components/blocks-renderer"
-import Headings from "../components/headings"
+import React from 'react';
+import {useStaticQuery, graphql} from 'gatsby';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+import BlocksRenderer from '../components/blocks-renderer';
+import Headings from '../components/headings';
 
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-
+import {GatsbyImage, getImage} from 'gatsby-plugin-image';
 
 const Me = () => {
+  //   const { strapiAbout } = useStaticQuery(graphql`
+  //     query {
+  //       strapiAbout {
+  //         title
+  //         blocks {
+  //           ...Blocks
+  //         }
+  //       }
+  //     }
+  //   `)
 
-
-
-//   const { strapiAbout } = useStaticQuery(graphql`
-//     query {
-//       strapiAbout {
-//         title
-//         blocks {
-//           ...Blocks
-//         }
-//       }
-//     }
-//   `)
-
-  const {strapiMe} = useStaticQuery(graphql`
+  const {strapiMe} = useStaticQuery (graphql`
 
 
   query {
@@ -45,89 +41,111 @@ const Me = () => {
     }
 
 
-  `)
+  `);
 
-
-
-
-
-
-
-
-
-
-
-  const { title, stack } = strapiMe
-  console.log(stack);
+  const {title, stack} = strapiMe;
+  console.log (stack);
 
   const seo = {
     metaTitle: title,
     metaDescription: title,
-  }
+  };
 
   return (
-    
-    <Layout >
+    <Layout>
 
-        <div className="container max-w-4xl py-8">
+      <div className="container max-w-4xl py-8">
 
+        <Seo seo={seo} />
 
+        
+        <Headings  title={strapiMe.title} />
 
-      <Seo seo={seo} />
-      <Headings  title={strapiMe.title} />
+<div className='test100'>
 
+        <h4  style={{textAlign: 'center'}}>Test1</h4>
 
-
-
-      <h4
-      style={{ textAlign:"center" }}
-      >Test1</h4>
+</div>
 
 
-      <p className="line-clamp-2 mt-2 text-neutral-500"
-      style={{ textAlign:"center" }}
-      >
+  <div className='image-des'
+  
+  style={{display:"flex", alignItems:"center"}}
+
+  
+  >
+
+            <GatsbyImage
+
+            image={getImage(strapiMe.image?.localFile)}
+            // alt={strapiMe.image?.alternativeText}
+
+
+            style={{ 
+
+            // height: "500px", 
+            // width:"300px", 
+            width:"100rem",
+            marginLeft:"auto", 
+            marginRight:"50px", 
+            display:"block", 
+            // marginTop:"50px",
+            borderRadius:"10px",
+
+
+            }}
+
+
+            />  
+
+
+        <p
+          className="line-clamp-2 mt-2 text-neutral-500"
+          style={{textAlign: 'center', display:"contents", overflow:"visible", marginTop:"20px"}}
+          >
           {strapiMe.info}
         </p>
 
-
-      {/* <BlocksRenderer blocks={blocks} /> */}
-
+        {/* <BlocksRenderer blocks={blocks} /> */}
 
 
 
 
-        <GatsbyImage
-
-        image={getImage(strapiMe.image?.localFile)}
-        alt={strapiMe.image?.alternativeText}
-        style={{ height: "500px", width:"300px", marginLeft:"auto", marginRight:"auto", display:"block", marginTop:"50px" }}
-        />  
-
-            <div 
-            
-            style={{ 
-
-display:"inline-block", margin:"0.25rem", padding:"0.25rem 0.5rem", background:"red", color:"white"
+  </div>
 
 
-
-             }}
-
-            
-            >
-              {stack.map(item => {
-                return <span key={item.id}>{item.title}</span>
-              })}
-            </div>
-
-
-
-
+        <div
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          {stack.map (item => {
+            return (
+              <span
+                style={{
+                  display: 'inline-block',
+                  background: 'skyblue',
+                  color: 'var(--clr-grey-5)',
+                  margin: '0.25rem',
+                  padding: '0.25rem 0.5rem',
+                  borderRadius: 'var(--radius)',
+                  textTransform: 'uppercase',
+                  fontSize: '0.85rem',
+                  borderRadius: '10px',
+                  opacity: '0.8',
+                }}
+                key={item.id}
+              >
+                {item.title}
+              </span>
+            );
+          })}
         </div>
 
-    </Layout>
-  )
-}
+      </div>
 
-export default Me
+    </Layout>
+  );
+};
+
+export default Me;
