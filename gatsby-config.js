ff4ea32ecc9d92cc1655e06697d6d1a99c82e246
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-05-25 10:32:57 +0200

    add goole analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,32 @@ module.exports = {
   plugins: [
     'gatsby-plugin-gatsby-cloud',
 
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-1K55PTZR34", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+     
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          // head: false,
+          head: true,
+          // Setting this parameter is also optional
+
+        
+
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+
 
 
   
